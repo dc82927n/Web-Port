d84a57224b2d0d87ac4e28fc0688b2353686f9f1
David <dc82927n@gmail.com>
2019-04-21 19:11:21 -0400

    added carousel

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,41 @@
 
+
 $(document).ready(function() {
-    $('#slides').superslides()
-});
+
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000, 
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: [" ", "Web Developer", "Food Connoisseur", "Front-End Developer", "Full Stack Developer"],
+        typeSpeed: 120, 
+        loop: true, 
+        startDelay: 20, 
+        showCursor: false
+    }); 
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+
+
+}); 
 
 
 
