be92ad18f68228605abe3bf655dfa159958fd23e
David <dc82927n@gmail.com>
2019-04-29 19:29:21 -0400

    restore if needed

--- JS/script.js
+++ JS/script.js
@@ -18,24 +18,47 @@ $(document).ready(function() {
 
     $('.owl-carousel').owlCarousel({
         loop: true,
-        margin: 10,
-        nav: true,
+        items: 4,
         responsive: {
             0: {
                 items: 1
             },
-            600: {
-                items: 3
+            480: {
+                items: 2
             },
-            1000: {
-                items: 5
+            768: {
+                items: 3
+            }, 
+            938: {
+                item: 4
             }
+        }
+    });
+    
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    
+    $(window).scroll(function(){
+        if(window.pageYOffset  > skillsTopOffset - $(window).height() + 300) {
+            console.log(skillsTopOffset)
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: "#c0392b",
+                trackColor: "#ffffff",
+                scalecColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function (from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
         }
     });
 
 
+});
 
-}); 
+
 
 
 
